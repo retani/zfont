e0d266078f34064f65f706eeb2e810a0b189f695
james <jamesdaniel2197@gmail.com>
2019-07-02 15:39:59 +0100

    deploy demo page

--- zfont.js
+++ zfont.js
@@ -6,6 +6,7 @@
  * github.com/jaames/zfont
  */
 
+(function(l, i, v, e) { v = l.createElement(i); v.async = 1; v.src = '//' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1'; e = l.getElementsByTagName(i)[0]; e.parentNode.insertBefore(v, e)})(document, 'script');
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
@@ -607,6 +608,12 @@
     },
     version: "1.2.4",
   };
+
+  {
+    // add dev server flag to the window object
+    // this won't be output in production mode
+    window.isDevServer = true;
+  }
 
   return index;
 
